e76a1ed37503d0ee269a8b1065518fb16de9e75c
Sheiger <emersonquispe2808@gmail.com>
2026-08-26 08:12:15 -0500

    ⚡️ add cart view

--- src/components/pages/desserts.tsx
+++ src/components/pages/desserts.tsx
@@ -25,10 +25,10 @@ const Desserts = () => {
     };
 
     return (
-        <div className="max-w-7xl mx-auto p-6">
-            <h1 className="text-4xl font-bold text-amber-900 pb-8 pt-6">Desserts</h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="min-h-screen bg-orange-50">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-7xl mx-auto pt-16">
                 <main className="md:col-span-2">
+                    <h1 className="text-4xl font-bold text-amber-900 pb-8">Desserts</h1>
                     <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4 gap-y-8"> 
                         { products.map((data) => (
                             <DessertCard key={data.id} 
@@ -41,7 +41,14 @@ const Desserts = () => {
                 </main>
 
                 <aside className="md:col-span-1">
-
+                    <div className="bg-white p-4">
+                        <h2 className="text-2xl font-bold text-amber-600">You Cart (0)</h2>
+                        <div className="flex flex-col items-center">
+                            <img src="https://images.vexels.com/media/users/3/322018/isolated/preview/90edee33f22270e889e6bb4fe2dc7962-gran-pastel-de-cumpleanos-dibujos-animados-retro.png" alt="Carrito vacio"></img>
+                            <span className="font-semibold text-gray-400">Your added items will appear here</span>
+                        </div>
+                        
+                    </div>
                 </aside>
 
             </div>     
