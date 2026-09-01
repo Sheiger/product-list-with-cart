5142886dc71211dd7560452ab568d247fcc7608c
Sheiger <emersonquispe2808@gmail.com>
2026-09-01 09:45:01 -0500

    ✨ add comment schema

---
+++ src/schemas/commentSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const commentSchema = z.object({
+    id: z.number().optional(),
+    productId: z.number(),
+    author: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
+    content: z.string().min(10, "El comentario debe ser mas descriptivo"),
+    reting: z.number().min(1).max(5)
+})
+
+export type CommentFormValues = z.infer<typeof commentSchema>;
